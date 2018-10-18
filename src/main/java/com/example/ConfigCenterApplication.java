83f147652e5b323deece30da39e831d694f2eff6
cuiyaqiang <cuiyaqiang1025@163.com>
2018-10-18 14:45:40 +0800

    代码提交

--- src/main/java/com/example/ConfigCenterApplication.java
+++ src/main/java/com/example/ConfigCenterApplication.java
@@ -2,11 +2,15 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableConfigServer
 @SpringBootApplication
 public class ConfigCenterApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ConfigCenterApplication.class, args);
+//		SpringApplication.run(ConfigCenterApplication.class, args);
+		new SpringApplicationBuilder(ConfigCenterApplication.class).web(true).run(args);
 	}
 }
